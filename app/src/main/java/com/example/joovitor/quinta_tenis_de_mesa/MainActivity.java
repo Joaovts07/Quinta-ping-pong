719b5debff31df4fe03aba2cea898f39aa4e7ae6
JOÃO VITOR donald <Joo Vitor>
2019-05-26 15:17:02 -0300

    first commit

---
+++ app/src/main/java/com/example/joovitor/quinta_tenis_de_mesa/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.joovitor.quinta_tenis_de_mesa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
